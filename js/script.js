a05a3326c62ca07b291a8067d3555de61dd39a3c
ArtoghrolGhazyII <developer.ahmed.web@gmail.com>
2021-01-07 05:11:39 +0200

    0.5 done

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+$("#btnopen").click(function () {
+    $("#main").animate({ left: "0" }, 500);
+    $("#btnclose").css("display", "block")
+    $("#btnopen").css("display", "none")
+    // $(".item").animate(500)
+    })
+    
+
+
+
+$("#btnclose").click(function () {
+    $("#main").animate({ left: "-250" }, 500);
+    $("#btnclose").css("display", "none")
+    $("#btnopen").css("display", "block")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
